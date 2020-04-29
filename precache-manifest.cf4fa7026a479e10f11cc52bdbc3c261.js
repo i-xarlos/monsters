e4dcd7222be0dac43b3b06db028c6a3269ca4787
Carlos <ixarlos@gmail.com>
2020-04-29 11:07:51 -0500

    Updates

---
+++ precache-manifest.cf4fa7026a479e10f11cc52bdbc3c261.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee8601078369ea4c511cc784ccac3597",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "fdb9b7e4c1d4a40b55ec",
+    "url": "/monsters/static/css/main.6aff0c1c.chunk.css"
+  },
+  {
+    "revision": "76e1e33d0dc86b7a1eb3",
+    "url": "/monsters/static/js/2.2e94f083.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.2e94f083.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdb9b7e4c1d4a40b55ec",
+    "url": "/monsters/static/js/main.146db4c2.chunk.js"
+  },
+  {
+    "revision": "72caaeccd1b1a5d2c0b1",
+    "url": "/monsters/static/js/runtime-main.68aaef51.js"
+  }
+]);
