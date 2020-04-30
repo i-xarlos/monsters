745ce396b5d11d8d9094e1324ff78f0d448b4e55
Carlos <ixarlos@gmail.com>
2020-04-30 10:51:54 -0500

    update

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends Component {
 	componentDidMount() {
 		fetch('https://jsonplaceholder.typicode.com/users')
 			.then((response) => response.json())
+			.catch((error) => console.log(error))
 			.then((users) => this.setState({ users }));
 	}
 
